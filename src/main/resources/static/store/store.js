960b5115678780351e3050048bfae10cce448ba1
u.malichenko <u.malichenko@gmail.com>
2020-10-06 15:42:26 +0300

    dz lesson7 черновой вариант, если успею то подправлю пагинацию

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -22,11 +22,26 @@ angular.module('app').controller('storeController', function ($scope, $http) {
     $scope.submitCreateNewProduct = function () {
         $http.post(contextPath + '/api/v1/products', $scope.newProduct)
             .then(function (response) {
-                // $scope.Products.push(response.data);
                 $scope.newProduct = null;
                 $scope.fillTable();
             });
     };
+
+    $scope.submitApplyNewFilter = function () {
+        $http({
+            url: contextPath + '/api/v1/products',
+            method: 'GET',
+            params: {
+                p: $scope.p != null ? $scope.p : 1,
+                title: $scope.newFilter != null ? $scope.newFilter.title : '',
+                min_price: $scope.newFilter != null ? $scope.newFilter.min_price : '',
+                max_price: $scope.newFilter != null ? $scope.newFilter.max_price : ''
+            }
+        }).then(function (response) {
+            $scope.Products = response.data;
+        });
+    };
+
 
     $scope.fillTable();
 });
